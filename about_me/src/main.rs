5a6a97d48683cf4d9c9b90a2b67517b018a3f3df
Gerard Causse <grod.causse@gmail.com>
2025-05-06 17:28:53 -0400

    Complete Project lesson

---
+++ about_me/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    /*
+    To compile, either run rustc ./main.rs or cargo build
+     */
+    print!("I am a programmer");
+    print!("I am learning Rust");
+    print!("🦀🦀🦀🦀🦀🦀🦀🦀");
+}
