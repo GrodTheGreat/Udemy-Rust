4e000a8c7625be7e1d173ee418b3dcc4d9f40fc5
Gerard Causse <grod.causse@gmail.com>
2025-05-06 18:27:04 -0400

    Complete Interpolation with Curly Braces lesson

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -2,4 +2,6 @@ fn main() {
     let apples = 50;
     let oranges = 14 + 6;
     let fruits = apples + oranges;
+    
+    println!("This year, my garden has {apples} apples and {oranges} oranges.");
 }
