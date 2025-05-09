437c40c5da06fedbdae21369589724634fdbb74a
Gerard Causse <grod.causse@gmail.com>
2025-05-08 20:11:52 -0400

    Complete Compiler Directives lesson

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,7 +1,8 @@
+#![allow(unused_variables)]
+
 type Meters = i32;
 
 fn main() {
     let mile_race_length: Meters = 1600;
     let two_mile_race_length: Meters = 3200;
-    println!("A one mile lenght race is{mile_race_length} and a two mile race length is {two_mile_race_length}");
 }
